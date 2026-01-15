aa28c09ed53bbc8c4e464b4e35f1ed8743a79e60
Felix Prünte <47022595+failllix@users.noreply.github.com>
2026-01-15 16:39:08 +0100

    Feature: Accept only single and improve UX

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -4,16 +4,19 @@ export default function Button({
   children,
   onClick,
   type,
+  disabled,
 }: {
   children: React.ReactNode;
   onClick?: () => void;
   type: "button" | "submit" | "reset";
+  disabled?: boolean;
 }) {
   return (
     <button
       type={type}
       onClick={onClick}
-      className="px-4 py-2 border rounded-lg"
+      disabled={disabled}
+      className={`px-4 py-2 border rounded-lg ${disabled && "opacity-50"}`}
     >
       {children}
     </button>
--- app/page.tsx
+++ app/page.tsx
@@ -4,55 +4,54 @@ import type React from "react";
 import { type ChangeEvent, useEffect, useRef, useState } from "react";
 import Button from "./components/button";
 
-interface FileError {
-  fileName: string;
-  message: string;
+interface DropZoneText {
+  text: string;
+  isError: boolean;
 }
 
 export default function Home() {
-  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
-  const [fileErrors, setFileErrors] = useState<FileError[]>([]);
+  const dropZoneDefaultText =
+    "Drop your .step or .stp file here, or click to upload.";
+  const [dropZoneText, setDropZoneText] = useState<DropZoneText>({
+    text: dropZoneDefaultText,
+    isError: false,
+  });
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const dropZoneRef = useRef<HTMLLabelElement>(null);
 
-  function validateAndUpdateSelectedFiles(files: File[]) {
-    setFileErrors([]);
-    setSelectedFiles([]);
+  function validateAndUpdateSelectedFiles(file: File) {
+    setSelectedFile(null);
 
-    const updatedFileErrors: FileError[] = [];
-
-    const validatedFiles = files
-      .filter((file) => {
-        if (file.name.endsWith(".step") || file.name.endsWith(".stp")) {
-          return true;
-        }
-
-        updatedFileErrors.push({
-          fileName: file.name,
-          message: `File type${file.name.split(".").at(-1)} is not supported`,
-        });
-
-        return false;
-      })
-      .filter((file) => {
-        if (file.size < 50_000_000) {
-          return true;
-        }
-
-        updatedFileErrors.push({
-          fileName: file.name,
-          message: "Filesize is larger than 50MB",
-        });
-
-        return false;
+    if (!(file.name.endsWith(".step") || file.name.endsWith(".stp"))) {
+      setDropZoneText({
+        text: "File must be of type .step or .stp",
+        isError: true,
       });
+      return;
+    }
 
-    setFileErrors(updatedFileErrors);
-    setSelectedFiles(validatedFiles);
+    if (file.size > 50_000_000) {
+      setDropZoneText({
+        text: "Filesize must be smaller than 50MB",
+        isError: true,
+      });
+      return;
+    }
+
+    setDropZoneText({
+      text: file.name,
+      isError: false,
+    });
+    setSelectedFile(file);
   }
 
   function changeHandler(event: ChangeEvent<HTMLInputElement>) {
     const files = event.target.files ? Array.from(event.target.files) : [];
-    validateAndUpdateSelectedFiles(files);
+    if (files.length > 1) {
+      return;
+    }
+
+    validateAndUpdateSelectedFiles(files[0]);
     event.target.value = "";
   }
 
@@ -62,12 +61,16 @@ export default function Home() {
       .map((item) => item.getAsFile())
       .filter((file): file is File => file !== null);
 
-    validateAndUpdateSelectedFiles(files);
+    if (files.length > 1) {
+      return;
+    }
+
+    validateAndUpdateSelectedFiles(files[0]);
   }
 
   function clearButtonHandler(): void {
-    setSelectedFiles([]);
-    setFileErrors([]);
+    setSelectedFile(null);
+    setDropZoneText({ text: dropZoneDefaultText, isError: false });
   }
 
   useEffect(() => {
@@ -92,6 +95,12 @@ export default function Home() {
             event.dataTransfer.dropEffect = "none";
           }
         }
+
+        if (fileItems.length > 1) {
+          if (event.dataTransfer != null) {
+            event.dataTransfer.dropEffect = "none";
+          }
+        }
       }
     };
 
@@ -109,10 +118,12 @@ export default function Home() {
       <h1>File upload</h1>
       <label
         ref={dropZoneRef}
-        className="mt-8 border-2 flex items-center justify-center border-amber-400 h-80 cursor-pointer rounded-lg"
+        className={`mt-8 border-2 flex items-center justify-center border-amber-400 h-80 cursor-pointer rounded-lg ${
+          dropZoneText.isError && "text-(--error)"
+        }`}
         onDrop={dropHandler}
       >
-        Drop .step or .stp files here, or click to upload.
+        {dropZoneText.text}
         <input
           className="hidden"
           onChange={changeHandler}
@@ -121,35 +132,16 @@ export default function Home() {
         />
       </label>
 
-      {selectedFiles.length > 0 && (
-        <div className="mt-8">
-          <h2>Selected Files</h2>
-          <ul>
-            {selectedFiles.map((file) => (
-              <li key={file.name}>{file.name}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-
-      {fileErrors.length > 0 && (
-        <div className="mt-8">
-          <h2>Errors</h2>
-          <ul className="text-(--error)">
-            {fileErrors.map((error) => (
-              <li key={error.fileName}>
-                {error.fileName}: {error.message}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-
       <div className="mt-8 flex gap-4">
         <Button type="button" onClick={clearButtonHandler}>
           Clear
         </Button>
-        <Button type="button">Configure Materials</Button>
+        <Button
+          type="button"
+          disabled={selectedFile == null || dropZoneText.isError}
+        >
+          Configure Materials
+        </Button>
       </div>
     </main>
   );
