eee91ef0c194d3db46b37cc80e5f311a0341b61f
Felix Prünte <47022595+failllix@users.noreply.github.com>
2026-01-17 01:39:23 +0100

    Fix: Backend build

--- db/db.ts
+++ db/db.ts
@@ -2,11 +2,7 @@ import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
-import { fileURLToPath } from "node:url";
 import path from "node:path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 config({ path: path.resolve(__dirname, "../.env.local") });
 
